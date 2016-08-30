d9a26a18addf6fe2d69d61d47a86c1050544f95c
Rebecca Elliff <rme9@students.uwf.edu>
2016-08-30 15:37:32 -0500

    Update myShell.c

--- myShell.c
+++ myShell.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int prompt()
 {
 	char *buffer = NULL;
-	
+	char *check = "exit";
 	printf("Please Enter Command: ");
 
 	scanf("%s", buffer);
+
+	if(strcmp(check,buffer)!= 0) return 0;
+	else return 1;
+}
+
+int main()
+{
+	prompt();
 	
-	if(buffer != NULL)//Exit condition
-	{
-		return 0;
-	}
-	else
-	{
-		//Launch
-	}
 	
 
-
-	return 0; 
+	
 }
