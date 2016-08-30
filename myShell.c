05df13b02a21f2dff7ff73f2f500fc73209e30bb
nam27 <nam27@students.uwf.edu>
2016-08-30 15:47:39 -0500

    Update myShell.c

--- myShell.c
+++ myShell.c
@@ -6,11 +6,12 @@ int prompt()
 {
 	char *buffer = NULL;
 	char *check = "exit";
+	
 	printf("Please Enter Command: ");
 
 	scanf("%s", buffer);
 
-	if(strcmp(check,buffer) == 0) return 0;
+	if(strcmp(check,buffer)!= 0) return 0;
 	else return 1;
 }
 
