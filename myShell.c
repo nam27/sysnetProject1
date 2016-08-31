39da98ba546be4ddae73cee2ed2b5899ace72d0a
Rebecca Elliff <rme9@students.uwf.edu>
2016-08-30 20:32:32 -0500

    Update myShell.c

--- myShell.c
+++ myShell.c
@@ -2,24 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-int prompt()
-{
-	char *buffer = NULL;
-	char *check = "exit";
-	
-	printf("Please Enter Command: ");
-
-	scanf("%s", buffer);
-
-	if(strcmp(check,buffer)!= 0) return 0;
-	else return 1;
-}
 
 int main()
 {
-	prompt();
+	int i = 0;
+	char *buffer;
+	char *check = "exit\n";
+	size_t bufsize = 100;
+
+	buffer = (char *)malloc(sizeof(char)*bufsize);
+
+	while(i == 0)
+	{
+		
+		printf("Please Enter Command: ");
+
+		fgets(buffer, bufsize, stdin);
+
+		printf("%s\n", buffer);
+
+		if(strcmp(check,buffer)== 0)
+		{
+			i = -1;
+		}
+		else
+		{
+			printf("Run...\n");			
+		}
+	}
+
 	
-	
+	free(buffer);
 
 	
 }
